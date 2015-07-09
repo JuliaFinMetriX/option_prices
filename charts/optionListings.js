0a0b8892b7dbe28baec6055bcd4229666ca6be9b
Christian Groll <groll.christian.edu@gmail.com>
2015-07-09 11:18:07 +0200

    option listings chart finished

--- charts/optionListings.js
+++ charts/optionListings.js
@@ -28,6 +28,27 @@ var yAxis = d3.svg.axis()
     .orient("left")
     .ticks(5);
 
+// function for the x grid lines
+function make_x_axis() {
+    return d3.svg.axis()
+        .scale(x)
+        .orient("bottom")
+        .ticks(5)
+}
+
+// function for the y grid lines
+function make_y_axis() {
+    return d3.svg.axis()
+        .scale(y)
+        .orient("left")
+        .ticks(5)
+}
+
+var DAXline = d3.svg.line()
+    .interpolate("basis")
+    .x(function(d) { return x(d.values[0].Date); })
+    .y(function(d) { return y(d.values[0].DAX); });
+
 // parse dates and remove missing values
 var parseDate = d3.time.format("%Y-%m-%d").parse,
 bisectDate = d3.bisector(function(d) { return d.values[0].Date; }).left; 
@@ -38,7 +59,8 @@ var tsdata = d3.csv("../data/chart_data/listingData.csv", function (d) {
 		  Date: new Date(d.Date),
 		  Expiry: new Date(d.Expiry),
 		  Strike: +d.Strike,
-		  IsCall: d.IsCall
+		  IsCall: d.IsCall,
+		  DAX: +d.DAX
 	 };
 }, function(error, tsdata) {
 	 
@@ -64,6 +86,32 @@ var tsdata = d3.csv("../data/chart_data/listingData.csv", function (d) {
 		  .attr("class", "x axis")
 		  .attr("transform", "translate(0," + height + ")")
 		  .call(xAxis);
+
+	 // append background
+	 svg.append("g")
+        .attr("class", "background")
+        .append("rect")
+        .attr("x", 0)
+        .attr("y", 0)
+        .attr("width", width)
+        .attr("height", height)
+
+	 // Draw the x Grid lines
+    svg.append("g")
+        .attr("class", "grid")
+        .attr("transform", "translate(0," + height + ")")
+        .call(make_x_axis()
+              .tickSize(-height, 0, 0)
+              .tickFormat("")
+             )
+    
+    // Draw the y Grid lines
+    svg.append("g")            
+        .attr("class", "grid")
+        .call(make_y_axis()
+              .tickSize(-width, 0, 0)
+              .tickFormat("")
+             )
 	 
 	 var yAxisWithoutTitle = svg.append("g")
 		  .attr("class", "y axis")
@@ -96,7 +144,9 @@ var tsdata = d3.csv("../data/chart_data/listingData.csv", function (d) {
         .attr("width", width)                              // **********
         .attr("height", height)                            // **********
         .style("fill", "none")                             // **********
-        .style("pointer-events", "all")                    // **********
+        .style("pointer-events", "all")
+        .on("mouseover", function() { focus.style("display", null); })
+        .on("mouseout", function() { focus.style("display", "none"); })
         .on("mousemove", mousemove);                       // **********
 
 	 	 yAxisWithoutTitle.append("text")
@@ -106,6 +156,30 @@ var tsdata = d3.csv("../data/chart_data/listingData.csv", function (d) {
 		  .style("text-anchor", "end")
 		  .text("price");
 
+	 svg.append("path")
+        .datum(uniqueDates)
+        .attr("class", "line")
+        .attr("d", DAXline);
+
+	 var focus = svg.append("g")
+        .attr("class", "focus")
+        .style("display", "none");
+    
+    focus.append("circle")
+        .attr("r", 4.5);
+    
+    focus.append("text")
+        .attr("x", -40)
+	     .attr("y", -15)
+        .attr("dy", ".35em");
+
+	 svg.append("line")
+	 	  // .attr("x1", x.range()[0])
+		  // .attr("y1", y.range()[0]/2)
+		  // .attr("x2", x.range()[1])
+		  // .attr("y2", y.range()[0]/2)
+	 	  .attr("class", "horzLine");
+	 
     function mousemove() {                                 // **********
         var x0 = x.invert(d3.mouse(this)[0]),
         i = bisectDate(uniqueDates, x0);
@@ -118,6 +192,16 @@ var tsdata = d3.csv("../data/chart_data/listingData.csv", function (d) {
 				d1 = uniqueDates[i],                                  // **********
 				dateInd = x(x0) - x(d0.values[0].Date) > x(d1.values[0].Date) - x(x0) ? i : (i-1);
 		  }
+
+		  // DAX tooltip
+		  focus.attr("transform", "translate(" + x(uniqueDates[dateInd].values[0].Date) + "," + y(uniqueDates[dateInd].values[0].DAX) + ")");
+        focus.select("text").text(uniqueDates[dateInd].values[0].DAX);
+
+		  svg.select(".horzLine")
+				.attr("x1", x(uniqueDates[dateInd].values[0].Date))
+				.attr("y1", y(uniqueDates[dateInd].values[0].DAX))
+		  		.attr("x2", x.range()[1])
+				.attr("y2", y(uniqueDates[dateInd].values[0].DAX));
 
 		  // make all points class invis
 		  circles.classed("vis", false)
